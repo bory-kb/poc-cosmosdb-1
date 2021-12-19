e84cb5b9aee1c09e78243793e5bdc97d026b9d24
bory_kb <user2@example.com>
2021-12-19 23:03:37 +0900

    console_wirte_todoitem

--- poc-cosmosdb-1/poc-cosmosdb-1/Service.cs
+++ poc-cosmosdb-1/poc-cosmosdb-1/Service.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.Cosmos.Fluent;
+
 namespace poc_cosmosdb_1
 {
     internal class Service
     {
 
-        private static void Main() {
+        private static async Task Main()
+        {
 
             var builder = new ConfigurationBuilder()
                 .AddUserSecrets<Service>()
@@ -17,7 +21,38 @@ namespace poc_cosmosdb_1
 
             var ConnectionString = builder["ConnectionString"];
 
-            Console.WriteLine($"{builder["ConnectionString"]}");
+            var cosmosClient = new CosmosClient(ConnectionString, new CosmosClientOptions
+            {
+                SerializerOptions = new CosmosSerializationOptions
+                {
+                    PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+                }
+            });
+
+            var container = cosmosClient.GetContainer("TodoList", "TodoItem");
+
+            var response = await container.ReadItemAsync<TodoItem>("9c3519b6-8a18-4eca-b034-1045f0b2d7e7",
+                new PartitionKey("bory")
+                );
+
+            var todoItem = response.Resource;
+
+            Console.WriteLine($"{todoItem.Title},{todoItem.Body},{todoItem.User.Name}");
         }
+
+    }
+
+    public class TodoItem
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public User User { get; set; }
+    }
+
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
     }
 }
