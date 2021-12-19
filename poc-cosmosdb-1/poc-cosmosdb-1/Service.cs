1f2a17afa35dd19213718f54f7e783e2dc695f01
user2 <user2@example.com>
2021-12-19 22:26:26 +0900

    secretSettings

---
+++ poc-cosmosdb-1/poc-cosmosdb-1/Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+namespace poc_cosmosdb_1
+{
+    internal class Service
+    {
+
+        private static void Main() {
+
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Service>()
+                .Build();
+
+            var ConnectionString = builder["ConnectionString"];
+
+            Console.WriteLine($"{builder["ConnectionString"]}");
+        }
+    }
+}
